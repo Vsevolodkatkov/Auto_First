673d3d9e9a0910e52f6a9905f1b40f35e1b77a91
vsevolod <vsevolodkatkov2121@gmail.com>
2022-04-09 18:49:48 +0300

    Start

--- src/com/kat/sech/Sech.java
+++ src/com/kat/sech/Sech.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Sech {
     public static void main(String[] args) {
-
+       System.out.println("hello");
         WebDriver obj = new ChromeDriver();
         obj.get("https://wwww.google.com");
     }
